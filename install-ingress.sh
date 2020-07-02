b05be660886c580115bf1a9a278683f8864da407
suren <suren>
2020-07-02 09:33:47 +0100

    added manifests

---
+++ install-ingress.sh
@@ -0,0 +1,5 @@
+helm install nginx-ingress stable/nginx-ingress \
+    --namespace ingress-demo \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
